45bf42973d861307e4a4370231db36eecce6c269
Jürgen Zornig <juergen.zornig@msgis.com>
2019-05-02 14:52:41 +0200

    run.cmd script

--- run.cmd
+++ run.cmd
@@ -1,17 +1,17 @@
 @echo off
-set GPKG="C:\Program Files\luciad\gpkg.exe"
-set QGIS_HOME="C:\Program Files\
-
-set /p QGIS_HOME="QGIS Home Verzeichnis, [ENTER] für Default [%QGIS_HOME%]: "
-ECHO %QGIS_HOME%
-
+cd /D "%~dp0"
+set QGIS_HOME=C:\Program Files\QGIS 3.6"
+set /p QGIS_HOME="QGIS Home Verzeichnis, druecke [ENTER] fuer Default [%QGIS_HOME%]: "
+set ENV="%QGIS_HOME%\bin\o4w_env.bat"
+echo %ENV%
+call %ENV%
 
 CHOICE /C YN /M "Download AGWR?"
 IF ERRORLEVEL 2 goto end
 IF ERRORLEVEL 1 GOTO download_agwr
 :download_agwr
-
-GWR\agwr2gpkg.cmd
+cd GWR
+call agwr2gpkg.cmd
 
 :end
 pause
