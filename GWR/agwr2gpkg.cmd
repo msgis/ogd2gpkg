7ac397ea372fcf8ef0e000a5f629a06cc6c4aad6
Jürgen Zornig <juergen.zornig@msgis.com>
2019-05-02 13:35:21 +0200

    GWR added ogr2ogr progress bar

--- GWR/agwr2gpkg.cmd
+++ GWR/agwr2gpkg.cmd
@@ -5,9 +5,9 @@ REM powershell -Command "Invoke-WebRequest -Uri 'http://www.bev.gv.at/pls/portal
 curl http://www.bev.gv.at/pls/portal/docs/PAGE/BEV_PORTAL_CONTENT_ALLGEMEIN/0200_PRODUKTE/UNENTGELTLICHE_PRODUKTE_DES_BEV/Adresse_Relationale_Tabellen-Stichtagsdaten.zip --output Adresse_Relationale_Tabellen-Stichtagsdaten.zip
 
 :importdata
-ogr2ogr -f GPKG ..\ogd.gpkg .\gwr_adr31256.vrt -overwrite -nln "gwr_adr" -s_srs "EPSG:31256" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
-ogr2ogr -f GPKG ..\ogd.gpkg .\gwr_adr31255.vrt -append -nln "gwr_adr" -s_srs "EPSG:31255" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
-ogr2ogr -f GPKG ..\ogd.gpkg .\gwr_adr31254.vrt -append -nln "gwr_adr" -s_srs "EPSG:31254" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
-ogr2ogr -f GPKG ..\ogd.gpkg .\gwr_geb31256.vrt -overwrite -nln "gwr_geb" -s_srs "EPSG:31256" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
-ogr2ogr -f GPKG ..\ogd.gpkg .\gwr_geb31255.vrt -append -nln "gwr_geb" -s_srs "EPSG:31255" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
-ogr2ogr -f GPKG ..\ogd.gpkg .\gwr_geb31254.vrt -append -nln "gwr_geb" -s_srs "EPSG:31254" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
+ogr2ogr -progress -f GPKG ..\ogd.gpkg .\gwr_adr31256.vrt -overwrite -nln "gwr_adr" -s_srs "EPSG:31256" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
+ogr2ogr -progress -f GPKG ..\ogd.gpkg .\gwr_adr31255.vrt -append -nln "gwr_adr" -s_srs "EPSG:31255" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
+ogr2ogr -progress -f GPKG ..\ogd.gpkg .\gwr_adr31254.vrt -append -nln "gwr_adr" -s_srs "EPSG:31254" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
+ogr2ogr -progress -f GPKG ..\ogd.gpkg .\gwr_geb31256.vrt -overwrite -nln "gwr_geb" -s_srs "EPSG:31256" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
+ogr2ogr -progress -f GPKG ..\ogd.gpkg .\gwr_geb31255.vrt -append -nln "gwr_geb" -s_srs "EPSG:31255" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
+ogr2ogr -progress -f GPKG ..\ogd.gpkg .\gwr_geb31254.vrt -append -nln "gwr_geb" -s_srs "EPSG:31254" -t_srs "EPSG:31287" -nlt POINT -oo ENCODING="cp1252"
